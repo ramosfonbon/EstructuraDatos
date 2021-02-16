19a267afd33757f72d0162a0cb79f5901c5ebc49
ramosfonbon <ramosfonbon@gmail.com>
2021-02-16 12:31:42 -0600

    Commit de prueba

--- src/complejidad/basicoGrafica/Main.java
+++ src/complejidad/basicoGrafica/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	    Complejidad c = new Complejidad();
 	    int ejecuciones = 20;
         int resultados[][] = new int[ejecuciones][2];
-/*
         for (int renglon=0; renglon < ejecuciones; renglon++) {
             int n = (renglon + 1) * 1;
             resultados[renglon][0] = n;
             resultados[renglon][1] = c.lineal(n);
         }
         imprimir(resultados);
+/*
 
         for (int renglon=0; renglon < ejecuciones; renglon++) {
             int n = (renglon + 1) * 1;
@@ -20,14 +20,20 @@ public class Main {
             resultados[renglon][1] = c.cuadratica(n);
         }
         imprimir(resultados);
-*/
+
+        /*
         for (int renglon=0; renglon < ejecuciones; renglon++) {
             int n = (renglon + 1) * 100;
             resultados[renglon][0] = n;
             resultados[renglon][1] = c.log(n);
         }
-        imprimir(resultados);
+        imprimir(resultados);  */
     }
+
+    private static void nada(){
+        // hacer algo
+    }
+
 
     private static void imprimir(int [][] resultado){
         for (int renglon = 0; renglon < resultado.length ; renglon++){
