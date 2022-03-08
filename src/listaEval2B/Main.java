d6408637ecc21ff3a1ee76d99a9c5562b458e2c2
vramos <ramosfonbon@gmail.com>
2022-03-08 13:16:03 -0600

    Agregado listaEval2B

---
+++ src/listaEval2B/Main.java
@@ -0,0 +1,25 @@
+package listaEval2B;
+
+import lista.ListaSimple;
+import lista.Recipiente;
+
+public class Main {
+    public static void main(String[] args) {
+        lista.ListaSimple<Persona> personas = new lista.ListaSimple<>();
+        // agregar 10 personas a la lista
+
+        // tomar la persona8 y ejecutar la busqueda, imprimir ls posicion en que se encontro
+        // ej: personas.contains(persona8)
+
+        // comparar la persona1 y la persona5, imprimir el resultado
+        // ej: persona1.compareTo(persona5)
+
+        imprimir(personas);
+
+    }
+
+    private static void imprimir(ListaSimple<Persona> personas) {
+
+
+    }
+}
