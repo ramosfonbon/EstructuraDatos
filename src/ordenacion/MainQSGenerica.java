aad547b197b6f59bf5f826b480d3be62f26e9730
vramos <ramosfonbon@gmail.com>
2022-03-22 15:11:52 -0600

    Agregado ordenacion generica

--- src/ordenacion/MainQSGenerica.java
+++ src/ordenacion/MainQSGenerica.java
@@ -14,11 +14,32 @@ public class MainQSGenerica {
         QuicksortGenerica<Recipiente> qsg = new QuicksortGenerica<>();
         qsg.quickSort(recipientes, 0, recipientes.length-1);
         imprimir(recipientes);
+
+        Integer[] numeros = { 534,946,17,	749,566,
+                552,304,732,230,374,
+                850,584,199,33,225,
+                159,437,809,183,383,
+                681,294,611,771,269,
+                561,902,508,720,830,
+                395,326,761,783,614,
+                606,278,892,129,882,
+                945,80,46,522,302,
+                669,70,706,268,999};
+
+        QuicksortGenerica<Integer> qsi = new QuicksortGenerica<>();
+        qsi.quickSort(numeros, 0, numeros.length-1);
+        imprimir(numeros);
     }
 
     private static void imprimir(Recipiente[] r){
         for (Recipiente recipiente:r){
             System.out.println(recipiente);
+        }
+    }
+
+    private static void imprimir(Integer[] r){
+        for (Integer entero:r){
+            System.out.println(entero);
         }
     }
 }
