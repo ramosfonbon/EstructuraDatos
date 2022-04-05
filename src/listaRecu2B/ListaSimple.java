434440734fec594bffa0a4a9e11be3f375746bb0
vramos <ramosfonbon@gmail.com>
2022-04-05 13:05:29 -0500

    Recuperacion B

---
+++ src/listaRecu2B/ListaSimple.java
@@ -0,0 +1,90 @@
+package listaRecu2B;
+
+public class ListaSimple<T> {
+    private Nodo<T> head;
+    private int contador;
+
+    public ListaSimple(){
+        head = new Nodo<>(null);
+        contador = 0;
+    }
+
+    public void add(T data) {
+        Nodo<T> temp = new Nodo<>(data);
+        Nodo<T> actual = head;
+        while (actual.getNext() != null) {
+            actual = actual.getNext();
+        }
+        actual.setNext(temp);
+        contador++;
+    }
+
+    public T get(int posicion){
+        if (posicion < 0){
+            return null;
+        }
+        if (posicion > contador){
+            return null;
+        }
+
+        Nodo<T> actual = head.getNext();
+        for (int i = 0; i < posicion; i++){
+            if (actual.getNext() != null) {
+                actual = actual.getNext();
+            }
+        }
+        return actual.getData();
+    }
+    // Implementar contains, regresa la posicion donde
+    // encuentre el primer objeto que sea igual
+    public int contains(T o){
+        for (int pos = 0; pos < size(); pos ++){
+            if (get(pos).equals(o)){
+                return pos;
+            }
+
+        }
+        return -1;
+
+    }
+
+
+
+
+    public int size(){
+        return contador;
+    }
+
+    public boolean isEmpty(){
+        return head.getNext() == null;
+    }
+
+
+    private class Nodo<T>{
+        private T data;
+        private Nodo next;
+
+        public Nodo(T data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Nodo getNext() {
+            return next;
+        }
+
+        public void setNext(Nodo next) {
+            this.next = next;
+        }
+    }
+}
+
+
---
+++ src/listaRecu2B/Main.java
@@ -0,0 +1,25 @@
+package listaRecu2B;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ListaSimple<Recipiente> Recipientes = new ListaSimple<>();
+
+        // agregar 10 Recipientes a la lista
+
+        // tomar la Recipiente4 y ejecutar la busqueda, imprimir ls posicion en que se encontro
+        // ej: Recipientes.contains(Recipiente4)
+
+        // comparar la Recipiente5 y la Recipiente8, imprimir el resultado
+        // ej: Recipiente5.compareTo(Recipiente8)
+
+
+        imprimir(Recipientes);
+
+    }
+
+    private static void imprimir(ListaSimple<Recipiente> Recipientes) {
+
+
+    }
+}
